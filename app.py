fc50ca2f77ef19f9db52d1b9323d4211c1d6f5c3
Thykof <nathan.seva@outlook.fr>
2018-08-30 22:03:01 +0200

    Change code to a flask app

--- app.py
+++ app.py
@@ -1,35 +1,29 @@
-from bottle import Bottle, run, template, request, static_file
 import requests
 
-app = Bottle()
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
 
 @app.route('/')
 def route():
-    return template('route')
+    return render_template('route.html')
 
-@app.post('/')
+@app.route('/', methods=['POST'])
 def url():
-    url = request.forms.get('url')
+    url = request.form.['url']
     if url == '':
-        return template('route')
+        return render_template('route.html')
     try:
         r = requests.get(url)
     except:
         error = "Invalid URL."
-        return template('route', error=error)
+        return render_template('route.html', error=error)
     if r.status_code == 200:
         urls = list()
         for elt in r.history:
             urls.append(elt.url)
         urls.append(r.url)
-        return template('route', urls=urls)
+        return render_template('route.html', urls=urls)
     else:
         error = "The request failed."
-        return template('route', error=error)
-
-@app.route('/style.css')
-def style():
-    return static_file('style.css', root='static')
-
-if __name__ == '__main__':
-    run(app, host='localhost', port=8080, debug=True, reloader=True)
+        return render_template('route.html', error=error)
