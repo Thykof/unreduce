a2a2b0f047d97a17c13862bc4707d5830010c62f
Thykof <nathan.seva@outlook.fr>
2018-08-30 23:50:30 +0200

    Make the website working

--- unreduce/home.py
+++ unreduce/home.py
@@ -1,28 +1,48 @@
+import requests
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from unreduce.db import get_db
 
-bp = Blueprint('home', __name__, url_prefix='/home')
+bp = Blueprint('home', __name__, url_prefix='/')
 
 
 
-@bp.route('/home', methods=('GET', 'POST'))
-def register():
+@bp.route('/', methods=('GET', 'POST'))
+def home():
     if request.method == 'POST':
         url = request.form['url']
+        db = get_db()
         error = None
+        urls = list()
 
         if not url:
             error = 'URL is required.'
-        elif url == "":
-            error = 'URL is empty.'
+        else:
+            url = url.strip()
+            if url == "":
+                error = 'URL is empty.'
 
         if error is None:
-            db.execute('INSERT INTO req (url) VALUES (?)', (url))
+            print(url)
+            #app.logger.debug('url: {}'.format(url))
+            db.execute('INSERT INTO req (url) VALUES (?)', (url,))
             db.commit()
-            return redirect(url_for('home.home'))
 
-        flash(error)
+            try:
+                r = requests.get(url)
+            except:
+                error = "Invalid URL."
+            else:
+                if r.status_code == 200:
+                    for elt in r.history:
+                        urls.append(elt.url)
+                    urls.append(r.url)
+                else:
+                    error = "The request failed."
+        if error is not None:
+            flash(error)
+        return render_template('results.html', urls=urls, error=error)
 
-    return render_template('auth/register.html')
+    return render_template('home.html')
